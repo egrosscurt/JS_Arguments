a0f1d73d98842c9c4776d3a49e4eab85f745041a
egrosscurt <84039948+egrosscurt@users.noreply.github.com>
2021-06-10 11:58:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Writing or defining a function
+const paintWall = function (wall, color) {
+    console.log("The " + wall " has been painted " + color);
+};
+
+const color = "red";
+const wall = "south-west";
+
+
+//Using or calling a function
+paintWall(wall, color);
+
+
